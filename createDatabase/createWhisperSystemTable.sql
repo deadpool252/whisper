943a1dbf5d389da7913e518c22a7202d2114a703
riko <2210450@ecc.ac.jp>
2023-05-12 13:46:49 +0900

    database create file up 2023/05/12 ueda

---
+++ createDatabase/createWhisperSystemTable.sql
@@ -0,0 +1,31 @@
+create table user(
+	userId varchar(30),
+	userName varchar(20) not null,
+	password varchar(64) not null,
+	profile varchar(200) dafault "",
+	iconPath varchar(100),
+	primary key(userId)
+);
+
+create table follow(
+	userId varchar(30),
+	followUserId varchar(30),
+	primary key(userId,followUserId)
+);
+
+create table whisper(
+	whisperNo bigint auto_incriment,
+	userId varchar(30), not null,
+	postDate date not null default now(),
+	content varchar(256), not null,
+	imagePath varchar(100) not,
+	primary key(whisperNo)
+);
+
+create table goodInfo(
+	userId varchar(30),
+	whispwerNo bigint,
+	primary key(userId,whisperNo)
+);
+
+
